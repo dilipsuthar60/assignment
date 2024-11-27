89a88e00c8a726a591baf802917cff33b0501896
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-11-27 15:30:05 +0530

    added proper images

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ export const bigFishData = [
     species: "Big Fish",
     name: "Humpback Whale",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXdEqEnkFITKgG008B0zuEWUBiqQW1D-TYzDeF1A8GRLs_-977nuw_DQHGil2Gfk6JK1ypSW0fm877tK5AJXONoePd3jEyVb9wqsooPg9G5zlbtr3Axkd12oGHyOJTI_V9-dT3G6Dg?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 15,
     location: "Atlantic Ocean",
   },
@@ -11,7 +11,7 @@ export const bigFishData = [
     species: "Big Fish",
     name: "Killer Whale",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXe-H9WQt2jsbIWN6_cnaeTWi4JRsWuymMSAshQgvNY7T4kJhDal02GtLGfZpLrBM-HVir1TtQ6hZlUx7hkD3105jVJaTNyJ5yOhNXwjVs5Jk9YTsic1HH17zjgSTlEYY4MVPDE1?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 12,
     location: "Atlantic Ocean",
   },
@@ -19,7 +19,7 @@ export const bigFishData = [
     species: "Big Fish",
     name: "Tiger Shark",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXcsqGI-7S-AJb8d5EhOmBu1WWmz3_F5zyBp2i9K5jbEzcO0inVj00ZuzpABaShKd1j4jE9x41R4HcLuzzjqm9g9uDsRac35KXqSSHpS3Sp7HJtwpb-kGy73UPjdedA-N0noSeKp?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 8,
     location: "Ocean",
   },
@@ -27,7 +27,7 @@ export const bigFishData = [
     species: "Big Fish",
     name: "Hammerhead Shark",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXcglfI037_bu4kb80cd8z_s2VP1AXkgSHRuoPqQi2-TtgpjUwDWE4FMn2yv4WsLmExd3jQ2S2B-KGww6RC2FtkQaQLhvhxjpTLZQx8bWrpbPfwiaxwsAz_eKRjPW53RDs6PQ7bo?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 8,
     location: "Ocean",
   },
@@ -38,7 +38,7 @@ export const dogsData = [
     species: "Dog",
     name: "Rotwailer",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXfypKhzBgatRBJl9wR9W6M4lrTbFXj3XantvUE2kpSr0jR6ugohd3nUd02HgT1mAf5csvGZvDMDfmYz3M7yw7nCPKM2rKtb5WOdboS2RYYli0fnduAP8biZDWE17rNqCrahCYo7?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 2,
     location: "Germany",
   },
@@ -46,7 +46,7 @@ export const dogsData = [
     species: "Dog",
     name: "German Shepherd",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXdbL1SU5IQRy8nmoGFFl5sqPse2zRsId-yIMBDSXa6NNRU0cQ7OkG8I0vZjDtgqswIG0TzXnWRpwMdL9riBn8PPdu5BXb-5NYIMh5_0V1aLIkdald9BYLgE6Fq6pWE7U7TfgnkiTQ?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 2,
     location: "Germany",
   },
@@ -54,7 +54,7 @@ export const dogsData = [
     species: "Dog",
     name: "Labrodar",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXcmCJ_-rMmfKoetFCcleNv1F668ucFL9gQ40rxS5_wcxEg0M0xSr6ymje6x5z1-__qVENjyOEJ6vvNQ4XOcAR1MT6kOf3SsQmYfbrz4RDwvArzRLtWTZ1tMktt-fHWl43lguB2Aew?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 2,
     location: "UK",
   },
@@ -62,7 +62,7 @@ export const dogsData = [
     species: "Dog",
     name: "Alabai",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXd2HUp65xM5RYz4ZzsHskPrIglKJ0AiSu0r9tZAf_aWJdaXUKKlIKwuEl3pV7lbSK0q8ogn3MajM8yyxqNxapJoTJH_AozLZSs-C2OgCmHUOjjTDXznl7G8gnvMorEeA_jDxraEcA?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 4,
     location: "Turkey",
   },
@@ -73,7 +73,7 @@ export const bigCatsData = [
     species: "Big Cats",
     name: "Tiger",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXc0_qzgYaCZ2_joclhSpj47SgLX7C60vYmeAInyCyJPPK-y8vx7NiBWUPKu0-PErYKMlgfzUIkmfPbuGXlTx5nm9MWrl2QD1NxjjZLbVQJq2OYU9Q1hHmw6LAnj2oWyMXFZtuBlcQ?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 10,
     location: "Asia",
   },
@@ -81,7 +81,7 @@ export const bigCatsData = [
     species: "Big Cats",
     name: "Lion",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXc00GLJ8N5IlnPhlOq7k0kIgqRVU603vy8VssVaNPyNkf2K6Zxr9vTbWZz4ahDI5FtbaDUzuVsoZyeOfbSFeTQQDS2krXfRO114X5ZDHcpF2I8uId2gBIBWw0VZd2TdELdLKfOSKg?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 8,
     location: "Africa",
   },
@@ -89,7 +89,7 @@ export const bigCatsData = [
     species: "Big Cats",
     name: "Leopard",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXe1NTVNmRIri1J3Dlr9XKzMzsgfz3Yio45dNcGDoIR1s2iqdt63yUi5THxpu-pvHmbekxnUd74JrAnlUmb_piT8iGhmJ7wtnbNumC2WGGygsu2bWsvBGMJqu41CC-YqYSmO1oCx?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 5,
     location: "Africa and Asia",
   },
@@ -97,7 +97,7 @@ export const bigCatsData = [
     species: "Big Cats",
     name: "Cheetah",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXcTDB2qYIQg0yLTWlt3IVIg-JyzxixMQ-mYk7OOrxXk671dlGmsxmRMIr06KDOdF2zDzPvbySmVMXYzBZGxcV5Lpq42WXb0m1oOcAltVdKyqx2-3-OtylRvamdFvD77yL-3U4Geug?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 5,
     location: "Africa",
   },
@@ -105,7 +105,7 @@ export const bigCatsData = [
     species: "Big Cats",
     name: "Caracal",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXdgB1CdecNPBAHb1DR7ULb3ZNlR-ks88ieXVfH8V9dhWUGxCskiyhH0Sw2hfw63HfIyZZ_BsxcAiXqZmq-iCTf8Dib8vo-K5lENrE8K4mmy-QQPQzHaynIcalHsW7aN93f1V4Fu1A?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 3,
     location: "Africa",
   },
@@ -113,7 +113,7 @@ export const bigCatsData = [
     species: "Big Cats",
     name: "Jaguar",
     image:
-      "https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/sheep-5.jpg",
+      "https://lh7-rt.googleusercontent.com/docsz/AD_4nXeSAJkEtGVE5YGuEPetAHKulTLbZ5PupZ51lCwMt07TXN1v_I76aAbW63aN9xyVGmTDoA6ero5GqRsY7HGIF2Fn6fRGhnGOb_FixdPVXb9VgYT7Duq_Wzqks_Hs29LtzMP0UHsQwg?key=diMK_80ckKTiDYYhCkLD1Q",
     size: 5,
     location: "Amazon",
   },
